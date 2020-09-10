3780bedc9b94f75cd4aa45360c3a1662ac8b5c2b
hyein-jeong <46813089+hyein-jeong@users.noreply.github.com>
2020-09-10 23:52:31 +0200

    Update main.js
    update main js for second day

--- data_includes/main.js
+++ data_includes/main.js
@@ -31,94 +31,80 @@ const replaceUploadingMessage = ()=>{
 window.requestAnimationFrame( replaceUploadingMessage );
 
 
-
 // Show the 'intro' trial first, then all the 'experiment' trials in a random order
 // then send the results and finally show the trial labeled 'bye'
+
+
 Sequence("intro_ID",
-	 "consent_form",
-	 "initiate_recorder",
-	 "audio_check",
-	 "questionnaire",	 
-	 
-"instruct_1_1_general",	 	
-"instruct_1_2_general",	 
-"preload_prac_cb",
+"consent_form",
+"initiate_recorder",
+"audio_check",
+"questionnaire",
+"instruct_1_1_day2_general",
+"instruct_1_2_day2_general",
 "instruct_2_prac_cblock",
+"preload_prac_cb",
 "prac_cb",
-"preload_pretrain_cb",
-"instruct_3_cblock_pretrain",
-"pretrain_cb",
-"instruct_4_pause_after_cblock_pretrain",
+"instruct_3_cblock_train1",
+"preload_ train1_cb ",
+randomize("train1_cb"),
+"instruct_4_pause_after_cblock_train1",	 
 "preload_prac_ncb",
 "instruct_5_prac_ncblock",
 "prac_ncb",
-"preload_pretrain_ncb",
-"instruct_6_ncblock_pretrain",
-"pretrain_ncb",
-"instruct_7_pause_after_ncblock_pretrain",
-"instruct_8_0_train",	 
-"preload_train1_cb",
-"instruct_8_1_cblock_train1",
-"train1_cb",
 "preload_train1_ncb",
-"instruct_8_1_ncblock_train1",
-"train1_ncb",	 
+"instruct_6_ncblock_train1",
+randomize("train1_ncb"),
+"instruct_7_pause_after_ncblock_train1",
 "preload_train2_cb",
 "instruct_8_2_cblock_train2",
-"train2_cb", 
+randomize("train2_cb"),
 "preload_train2_ncb",
 "instruct_8_2_ncblock_train2",
-"train2_ncb",
-"instruct_8_2_pause_after_ncblock_train2.png",
+randomize("train2_ncb"),
+"instruct_8_2_pause_after_ncblock_train2",
 "preload_train3_cb",
 "instruct_8_3_cblock_train3",
-randomize("train3_cb"),	 
+randomize("train3_cb"),
 "preload_train3_ncb",
 "instruct_8_3_ncblock_train3",
-randomize("train3_ncb"),	 
+randomize("train3_ncb"),
 "preload_train4_cb",
 "instruct_8_4_cblock_train4",
-"train4_cb",
+randomize("train4_cb"),
 "preload_train4_ncb",
 "instruct_8_4_ncblock_train4",
-"train4_ncb",
-"instruct_8_4_pause_after_ncblock_train4.png",
-"preload_train5_cb",
+randomize("train4_ncb"),
+"instruct_8_4_pause_after_ncblock_train4",
 "instruct_8_5_cblock_train5",
-"train5_cb",
-"preload_train5_ncb",
+"preload_train5_cb",
+randomize("train5_cb"),
 "instruct_8_5_ncblock_train5",
-"train5_ncb",
-"preload_train6_cb",
+"preload_train5_ncb",
+randomize("train5_ncb"),
 "instruct_8_6_cblock_train6",
-"train6_cb",
-"preload_train6_ncb",
+"preload_train6_cb",
+randomize("train6_cb"),
 "instruct_8_6_ncblock_train6",
-"train6_ncb",	 	 
-//"instruct_9_0_general_test",
-//"preload_test_cb",
-//"instruct_9_1_cblock_test",
-//"test_cb",	 
-//"preload_test_ncb",
-//"instruct_9_2_ncblock_test",
-//"test_ncb",	 
-"comment",
+"preload_train6_ncb",
+randomize("train6_ncb"),
+"instruct_8_6_pause_after_ncblock_train6",	 
+"instruct_9_0_general_test",
+"preload_test_cb",
+"instruct_9_1_cblock_test",
+"test_cb",
+"preload_test_ncb",
+"instruct_9_2_ncblock_test",
+"test_ncb",
+
 "send",
 "final");
-
-CheckPreloaded("test_sample", 5000)
-    .label("preload_test_sample");   
 
 CheckPreloaded("prac_cb", 5000)
     .label("preload_prac_cb");   
 CheckPreloaded("prac_ncb", 5000)
     .label("preload_prac_ncb");
  
-CheckPreloaded("pretrain_cb", 10000)
-    .label("preload_pretrain_cb");
-CheckPreloaded("pretrain_ncb", 10000)
-    .label("preload_pretrain_ncb");
-
 CheckPreloaded("train1_cb", 10000)
     .label("preload_train1_cb");
 CheckPreloaded("train1_ncb", 10000)
@@ -149,7 +135,6 @@ CheckPreloaded("train6_cb", 10000)
 CheckPreloaded("train6_ncb", 10000)
     .label("preload_train6_ncb");
 
-
 CheckPreloaded("test_cb", 10000)
     .label("preload_test_cb");
 
@@ -162,8 +147,6 @@ Template(GetTable("intro_recorder.csv"),
     InitiateRecorder("https://uni-potsdam.de/phraseproduction/exp2/upload-recording.php", ir.line1)
         .label("initiate_recorder")
 )
-
-
 
 Template(GetTable("intro_ID.csv"),
     iid =>
@@ -183,27 +166,6 @@ Template(GetTable("intro_ID.csv"),
             .wait()
     )
 );
-
-newTrial("intro_ID_sample",
-    defaultText
-        .print()
-    ,
-    newText("<p>Welcome!</p>"),
-    newText("<p>In this experiment, your task is to describe pictures in various ways.</p>"),
-    newText("<p>Please enter your ID</p>"),
-    newTextInput("inputID")
-        .print()
-    ,
-    newVar("ID")
-        .global()
-        .set( getTextInput("inputID") )
-    ,
-    newButton("Continue")
-        .print()
-        .wait()
-)
-.log( "ID" , getVar("ID") );
-
 
 Template(GetTable("consent_form.csv"),
     cf =>
@@ -225,7 +187,6 @@ Template(GetTable("consent_form.csv"),
             .wait()
     )
 );
-
 
 
 Template(GetTable("questionnaire.csv"),
@@ -250,58 +211,6 @@ Template(GetTable("questionnaire.csv"),
             .add("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50", "51", "52", "53", "54", "55", "56", "57", "58", "59", "60", "61", "62", "63", "64", "65", "66", "67", "68", "69", "70", "71", "72", "73", "74", "75", "76", "77", "78", "79", "80", "81", "82", "83", "84", "85", "86", "87", "88", "89", "90", "91", "92", "93", "94", "95", "96", "97", "98", "99", "100+")
             .print()
             .log()
-        ,
-        newText("line3", qu.line3)
-            .after(getDropDown("Age"))
-            .print()
-        ,
-        newTextInput("Language")
-            .size(100, 20)
-            .log()
-        ,
-        newText("line4", qu.line4)
-            .after(getTextInput("Language"))
-            .print()
-        ,
-        newImage("yes_hear", "resp_ja.png")
-            .size(30,15)
-        ,
-        newImage("no_hear", "resp_nein.png")
-            .size(30,15)
-        ,
-        newText("line5", qu.line5)
-            .after(getImage("yes_hear"))
-            .after(getImage("no_hear"))
-            .print()
-        ,
-        newSelector("Hearing")
-            .add(getImage("yes_hear") , getImage("no_hear"))
-            .select(getImage("no_hear"))
-            .log()
-        ,
-        newImage("yes_imp", "resp_ja.png")
-            .size(30,15)
-        ,
-        newImage("no_imp", "resp_nein.png")
-            .size(30,15)
-        ,
-        newText("line6", qu.line6)
-            .after(getImage("yes_imp"))
-            .after(getImage("no_imp"))
-            .print()
-        ,
-        newSelector("Impairments_yn")
-            .add(getImage("yes_imp") , getImage("no_imp"))
-            .select(getImage("no_imp"))
-            .log()
-        ,
-        newTextInput("Impairments")
-            .size(100, 20)
-            .log()
-        ,
-        newText("line7", qu.line7)
-            .after(getTextInput("Impairments"))
-            .print()
         ,
         newTextInput("Prolific_ID")
             .size(100, 20)
@@ -366,11 +275,11 @@ Template(GetTable("audio_check.csv"),
     )
 );
 
-newTrial("instruct_1_1_general",
+newTrial("instruct_1_1_day2_general",
     defaultText
         .print()
     ,
-    newImage("pic_instruct_1_general", "instruct_1_1_general.png")
+    newImage("pic_instruct_1_general", "instruct_1_1_day2_general.png")
         .size(1280, 720)
         .print()
     ,
@@ -379,11 +288,11 @@ newTrial("instruct_1_1_general",
         .wait()
 );
 
-newTrial("instruct_1_2_general",
+newTrial("instruct_1_2_day2_general",
     defaultText
         .print()
     ,
-    newImage("pic_instruct_1_general", "instruct_1_2_general.png")
+    newImage("pic_instruct_1_general", "instruct_1_2_day2_general.png")
         .size(1280, 720)
         .print()
     ,
@@ -406,11 +315,11 @@ newTrial("instruct_2_prac_cblock",
 );
 
 
-newTrial("instruct_3_cblock_pretrain",
+newTrial("instruct_3_cblock_train1",
     defaultText
         .print()
     ,
-    newImage("pic_instruct", "instruct_3_cblock_pretrain.png")
+    newImage("pic_instruct", "instruct_3_cblock_train1.png")
         .size(1280, 720)
         .print()
     ,
@@ -420,11 +329,11 @@ newTrial("instruct_3_cblock_pretrain",
 );
 
 
-newTrial("instruct_4_pause_after_cblock_pretrain",
+newTrial("instruct_4_pause_after_cblock_train1",
     defaultText
         .print()
     ,
-    newImage("pic_instruct", "instruct_4_pause_after_cblock_pretrain.png")
+    newImage("pic_instruct", "instruct_4_pause_after_cblock_train1.png")
         .size(1280, 720)
         .print()
     ,
@@ -448,11 +357,11 @@ newTrial("instruct_5_prac_ncblock",
 );
 
 
-newTrial("instruct_6_ncblock_pretrain",
+newTrial("instruct_6_ncblock_train1",
     defaultText
         .print()
     ,
-    newImage("pic_instruct", "instruct_6_ncblock_pretrain.png")
+    newImage("pic_instruct", "instruct_6_ncblock_train1.png")
         .size(1280, 720)
         .print()
     ,
@@ -461,52 +370,11 @@ newTrial("instruct_6_ncblock_pretrain",
         .wait()
 );
 
-newTrial("instruct_7_pause_after_ncblock_pretrain",
+newTrial("instruct_7_pause_after_ncblock_train1",
     defaultText
         .print()
     ,
-    newImage("pic_instruct", "instruct_7_pause_after_ncblock_pretrain.png")
-        .size(1280, 720)
-        .print()
-    ,
-    newKey("space", " ")
-	.log()
-        .wait()
-);
-
-
-newTrial("instruct_8_0_train",
-    defaultText
-        .print()
-    ,
-    newImage("pic_instruct", "instruct_8_0_train.png")
-        .size(1280, 720)
-        .print()
-    ,
-    newKey("space", " ")
-	.log()
-        .wait()
-);
-
-newTrial("instruct_8_1_cblock_train1",
-    defaultText
-        .print()
-    ,
-    newImage("pic_instruct", "instruct_8_1_cblock_train1.png")
-        .size(1280, 720)
-        .print()
-    ,
-    newKey("space", " ")
-	.log()
-        .wait()
-);
-
-
-newTrial("instruct_8_1_ncblock_train1",
-    defaultText
-        .print()
-    ,
-    newImage("pic_instruct", "instruct_8_1_ncblock_train1.png")
+    newImage("pic_instruct", "instruct_7_pause_after_ncblock_train1.png")
         .size(1280, 720)
         .print()
     ,
@@ -725,19 +593,6 @@ newTrial("instruct_9_2_ncblock_test",
         .wait()
 );
 
-newTrial("instruct_9_3_test_uploading",
-    defaultText
-        .print()
-    ,
-    newImage("pic_instruct", "instruct_9_3_test_uploading.png")
-        .size(1280, 720)
-        .print()
-    ,
-    newKey("space", " ")
-	.log()
-        .wait()
-);
-
 ////////////////////////////////////  templates for practice trials 
 
 Template(GetTable("prac_cblock.csv"),
@@ -841,108 +696,7 @@ Template(GetTable("prac_ncblock.csv"),
 );
 
 
-///////////////////// templates for trials during pretrain, train and test session 
-
-Template(GetTable("list1_pretrain_cblock.csv"),
-    pretrain_cb =>
-    newTrial("pretrain_cb",
-    
-    newImage("fixation_cross", "fixation.png")
-        .size(300, 300)
-        .print()
-        .log()
-    ,
-    newTimer("pretrain_fixation", 500)
-        .start()
-        .wait()
-    ,
-    getImage("fixation_cross")
-        .remove()
-    ,
-    newMediaRecorder("pretrain_recorder", "audio")
-        .hidden()
-        .record()
-        .log()
-    ,
-    newImage("pretrain_picture", pretrain_cb.picture_file)
-        .size(300, 300)
-        .print()
-    ,
-    newTimer("pretrain_trial", 2000)
-        .start()
-        .wait()
-        .log()
-    ,
-    getImage("pretrain_picture")
-        .remove()
-    ,
-    newTimer("post_trial", 1500)
-        .start()
-        .wait()
-        .log()
-    ,
-    getMediaRecorder("pretrain_recorder")
-        .stop()
-        .remove()
-	.log()    
-    )
-    .log( "sub_id"     , getVar("ID")    )
-    .log( "phrase_item", pretrain_cb.phrase_item )
-    .log( "phrase_pretrain", pretrain_cb.phrase_pretrain)
-    .log( "condition_exposure", pretrain_cb.condition_exposure)
-    .log( "condition_phrFreq", pretrain_cb.condition_phrFreq)
-);
-
-
-Template(GetTable("list1_pretrain_ncblock.csv"),
-    pretrain_ncb =>
-    newTrial("pretrain_ncb",
-    
-    newImage("fixation_cross", "fixation.png")
-        .size(300, 300)
-        .print()
-        .log()
-    ,
-    newTimer("pretrain_fixation", 500)
-        .start()
-        .wait()
-    ,
-    getImage("fixation_cross")
-        .remove()
-    ,
-    newMediaRecorder("pretrain_recorder", "audio")
-        .hidden()
-        .record()
-        .log()
-    ,
-    newImage("pretrain_picture", pretrain_ncb.picture_file)
-        .size(300, 300)
-        .print()
-    ,
-    newTimer("pretrain_trial", 2000)
-        .start()
-        .wait()
-        .log()
-    ,
-    getImage("pretrain_picture")
-        .remove()
-    ,
-    newTimer("post_trial", 1500)
-        .start()
-        .wait()
-        .log()
-    ,
-    getMediaRecorder("pretrain_recorder")
-        .stop()
-        .remove()
-	.log()    
-    )
-    .log( "sub_id"     , getVar("ID")    )
-    .log( "phrase_item", pretrain_ncb.phrase_item )
-    .log( "phrase_pretrain", pretrain_ncb.phrase_pretrain)
-    .log( "condition_exposure", pretrain_ncb.condition_exposure)
-    .log( "condition_phrFreq", pretrain_ncb.condition_phrFreq)
-);
+///////////////////// templates for trials during train and test session 
 
 Template(GetTable("list1_train1_cblock.csv"),
     train1_cb =>
@@ -1655,7 +1409,19 @@ Template(GetTable("feedback.csv"),
             newText(fb.line1)
                 .print()
             ,
-            newTextInput("feedback")
+	    newText(fb.line2)
+                .print()
+            ,
+	    newTextInput("feedback")
+                .settings.size(400, 50)
+                .css("border", "solid 2px grey")
+                .settings.log()
+                .print()
+            ,
+	    newText(fb.line9)
+                .print()
+            ,
+	    newTextInput("feedback")
                 .settings.size(400, 50)
                 .css("border", "solid 2px grey")
                 .settings.log()
@@ -1667,6 +1433,7 @@ Template(GetTable("feedback.csv"),
                 .wait()
         )
 )
+
 
 
 SendResults("send");
