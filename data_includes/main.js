3ab58c41e5c09180ba4c71d09b761bc5ae7ef621
hyein-jeong <46813089+hyein-jeong@users.noreply.github.com>
2020-11-21 16:48:09 +0100

    Update main.js

--- data_includes/main.js
+++ data_includes/main.js
@@ -144,7 +144,7 @@ CheckPreloaded("test_ncb", 10000)
 //start the recorder and send result files to the server
 Template(GetTable("intro_recorder.csv"),
     ir =>
-    InitiateRecorder("https://uni-potsdam.de/phraseproduction/exp2/upload-recording.php", ir.line1)
+    InitiateRecorder("https://uni-potsdam.de/phraseproduction/exp2_day2/upload-recording.php", ir.line1)
         .label("initiate_recorder")
 )
 
@@ -208,7 +208,7 @@ Template(GetTable("questionnaire.csv"),
             .print()
         ,
         newDropDown("Age", "--")
-            .add("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50", "51", "52", "53", "54", "55", "56", "57", "58", "59", "60", "61", "62", "63", "64", "65", "66", "67", "68", "69", "70", "71", "72", "73", "74", "75", "76", "77", "78", "79", "80", "81", "82", "83", "84", "85", "86", "87", "88", "89", "90", "91", "92", "93", "94", "95", "96", "97", "98", "99", "100+")
+            .add("18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50", "51", "52", "53", "54", "55", "56", "57", "58", "59", "60", "61", "62", "63", "64", "65", "66", "67", "68", "69", "70", "71", "72", "73", "74", "75", "76", "77", "78", "79", "80", "81", "82", "83", "84", "85", "86", "87", "88", "89", "90", "91", "92", "93", "94", "95", "96", "97", "98", "99", "100+")
             .print()
             .log()
         ,
@@ -226,7 +226,7 @@ Template(GetTable("questionnaire.csv"),
         ,
         newVar("ID")
             .global()
-            .set(getTextInput("Prolific_ID") )
+            .set(getTextInput("Prolific_ID"))
             .log()
         ,
         newButton("qu_test_button", "Fortfahren in den Vollbildmodus.")
@@ -1444,10 +1444,9 @@ Template(GetTable("feedback.csv"),
 )
 
 
-
 SendResults("send");
 
-Template(GetTable("final_test.csv"),
+Template(GetTable("final_test_day2.csv"),
     fin =>
         newTrial("final",
             exitFullscreen()
@@ -1458,15 +1457,4 @@ Template(GetTable("final_test.csv"),
             newButton("void")
                 .wait()
         )
-);
-
-newTrial("final_sample",
-    newText("<p>Thank you for your participation!</p>")
-        .print()
-    ,
-    newText("<p><a href='https://www.pcibex.net/'>Click here to validate your participation.</a></p>")
-        .print()
-    ,
-    newButton("void")
-        .wait()
 );
